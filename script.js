6ac0e167f321b42977fbbe64c49eb1f8095137c6
GuangYu-yu <77334277+GuangYu-yu@users.noreply.github.com>
2025-09-11 19:13:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -360,7 +360,7 @@ function handleSearch() {
     } else {
         // 搜索模式
         // 检查是否是网址
-        if (query.includes('.') && !query.includes(' ')) {
+        if (isURL(query)) {
             url = query.startsWith('http') ? query : 'https://' + query;
         } else {
             url = searchEngines[currentEngine] + encodeURIComponent(query);
@@ -368,6 +368,23 @@ function handleSearch() {
     }
     
     window.location.href = url;
+}
+
+// 判断是否是网址的函数
+function isURL(string) {
+    // 如果包含空格，不是网址
+    if (string.includes(' ')) {
+        return false;
+    }
+    
+    // 如果以http或https开头，很可能是网址
+    if (string.startsWith('http://') || string.startsWith('https://')) {
+        return true;
+    }
+    
+    // 检查是否符合网址格式（域名.顶级域名）
+    const urlPattern = /^[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.[a-zA-Z]{2,}([\/?#]|$)/;
+    return urlPattern.test(string);
 }
 
 // 设置主题
